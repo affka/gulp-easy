67e4a1c3db81e02389c20df673ec20d6012def10
Kozhin Vladimir <affka@affka.ru>
2016-03-10 18:06:32 +0700

    v0.1.11 npm release

--- src-es5-build/task/Base.js
+++ src-es5-build/task/Base.js
@@ -11,60 +11,60 @@ var _through2 = require('through2');
 var _through22 = _interopRequireDefault(_through2);
 
 var Base = (function () {
-  Base._noop = function _noop() {
-    return _through22['default'].obj(function (file, enc, cb) {
-      this.push(file);
-      cb();
-    });
-  };
+    Base._noop = function _noop() {
+        return _through22['default'].obj(function (file, enc, cb) {
+            this.push(file);
+            cb();
+        });
+    };
 
-  function Base(gulp, manager, name) {
-    _classCallCheck(this, Base);
+    function Base(gulp, manager, name) {
+        _classCallCheck(this, Base);
 
-    /**
-     * @type {module:gulp}
-     */
-    this.gulp = gulp;
+        /**
+         * @type {module:gulp}
+         */
+        this.gulp = gulp;
 
-    /**
-     * @type {Manager}
-     */
-    this.manager = manager;
+        /**
+         * @type {Manager}
+         */
+        this.manager = manager;
 
-    /**
-     * @type {string}
-     */
-    this.name = name;
+        /**
+         * @type {string}
+         */
+        this.name = name;
 
-    /**
-     * @type {string[]}
-     */
-    this.src = null;
+        /**
+         * @type {string[]}
+         */
+        this.src = null;
 
-    /**
-     * @type {{dir: string, file: string, ext: string, name: string}}
-     */
-    this.dest = null;
+        /**
+         * @type {{dir: string, file: string, ext: string, name: string}}
+         */
+        this.dest = null;
 
-    /**
-     * @type {object}
-     */
-    this.config = {};
-  }
+        /**
+         * @type {object}
+         */
+        this.config = {};
+    }
 
-  Base.prototype.init = function init() {};
+    Base.prototype.init = function init() {};
 
-  Base.prototype.run = function run() {};
+    Base.prototype.run = function run() {};
 
-  Base.prototype.isWatch = function isWatch() {
-    return this.manager.isWatch();
-  };
+    Base.prototype.isWatch = function isWatch() {
+        return this.manager.isWatch();
+    };
 
-  Base.prototype.isCompress = function isCompress() {
-    return this.manager.isCompress();
-  };
+    Base.prototype.isCompress = function isCompress() {
+        return this.manager.isCompress();
+    };
 
-  return Base;
+    return Base;
 })();
 
 exports['default'] = Base;
